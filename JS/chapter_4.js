133bbefba04dffb1b61e87655e5f5afaf20b3d56
arifrashidi <78742458+arifrashidi@users.noreply.github.com>
2022-12-16 10:48:46 +0800

    third_commit

--- JS/chapter_4.js
+++ JS/chapter_4.js
@@ -28,27 +28,104 @@ console.log("--- 📁 CHAPTER 4: DOM and Events ---");
 
 /* -------------------------------------------------------------------------- */
 
-// 🧡 querySelector() and querySelectorAll()
+// 🧡 HTML DOM DOCUMENT
 
 // -----------------
-// ❕ HTML DOM Document querySelector()
+// ❕ querySelector()
 // 📝 The querySelector() method returns the first element that matches a CSS selector.
-//TODO:📝 querySelector() syntax =
-/*
-    document.querySelector(CSS selectors)
-*/
-
-// 🔗 Example 1:
-document.querySelector(".example").textContent;
+//TODO = document.querySelector(CSS selectors)
 
 // -----------------
-// ❕ HTML DOM Document querySelectorAll()
+// ❕ querySelectorAll()
 // 📝 The querySelectorAll() method returns all matches (not only the first) that matches a CSS selector.
-//TODO:📝 querySelectorAll() syntax =
-/*
-    document.querySelectorAll(CSS selectors)
-*/
+//TODO = document.querySelectorAll(CSS selectors)
 
-// 🔗 Example 1:
-document.querySelectorAll("example").textContent;
+// -----------------
+// ❕ getElementById()
+// 📝 Returns an element with a specified value & returns null if the element does not exist.
+// 📝 Is More restrictive than querySelector() because you can only retrieve elements by its DOM ID.
+// 📝 GetElementById() is more faster in browser than querySelector()
+// TODO Syntax: document.getElementById("SELECTOR");
+
+/* -------------------------------------------------------------------------- */
+
+// 🧡 HTML DOM ELEMENT
+
+// -----------------
+// ❕ addEventListener()
+// 📝 attaches an event handler to an element.
+// TODO Syntax: ELEMENT.addEventListener(EVENT, FUNCTION, USECAPTURE);
+// 📝 event example: "click" / "mouseover" / "mouseout"
+// 📝 function: The function will executed when the event occurs.
+
+// ✨ (HTML DOM EVENT):
+
+    // 🔗 "click" ✨(MOUSE EVENT)
+    // 📝 Event occurs when the user clicks on an HTML element.
+    // TODO Syntax: ELEMENT.addEventListener("click", FUNCTION)
+
+    // 🔗 "keydown" ✨(KEYBOARD EVENT)
+    // 📝 Event occurs when the user is pressing a key (not fired continuously)
+    // TODO Syntax: ELEMENT.addEventListener("keydown", FUNCTION)
+
+    // 🔗 key ✨(HTML DOM EVENT - METHOD)
+    // 📝 Returns the input key that was pressed when a key event occured
+    // TODO Syntax: EVENT.key
+
+// -----------------
+// ❕ textContent
+// 📝 Change(HTML content) from the choosen selector, and all its descendants.
+// TODO Syntax: ELEMENT.textContent = "VALUE";
+
+// -----------------
+// ❕ style
+// 📝 Manipulating CSS style using Javascript:
+// TODO Syntax: ELEMENT.style.CSS_PROPERTY = "VALUE";
+
+// -----------------
+// ❕ classlist ✨(DOM TOKEN LIST)
+// 📝 Returns the CSS classnames of an element.
+// TODO Syntax: ELEMENT.classlist.METHOD
+
+// ✨ (DOM TOKEN LIST - METHOD):
+
+    // 🔗 classList.remove() 
+    // 📝 Removes one or more tokens from the list
+    // TODO Syntax: ELEMENT.classlist.remove("SELECTOR");
+
+    // 🔗classList.contain()
+    // 📝 Returns TRUE if a DOMTokenList contains a class, otherwise FALSE
+    // TODO Syntax:  ELEMENT.classlist.contain("SELECTOR");
+
+    // 🔗 classList.toggle() 
+    // 📝 Toggles between tokens in the list (like switch button)
+    // TODO Syntax:  ELEMENT.classlist.toggle("SELECTOR");
+
+/* -------------------------------------------------------------------------- */
+
+// 🧡 OBJECT
+
+// -----------------
+// ❕ Math
+// 📝 The JavaScript Math object allows you to perform mathematical tasks on numbers.
+// TODO Syntax: Math.METHOD
+
+// ✨ (MATH - METHOD):
+
+    // 🔗 Math.random() 
+    // 📝 Math.random() returns a random number between 0 and 1.
+    // TODO Syntax: Math.random()
+
+    // 🔗 Math.trunc()
+    // 📝 Returns the integer part of x:
+    // TODO Syntax: Math.trunc(NUMBER)
+
+/* -------------------------------------------------------------------------- */
+
+// 🧡 OTHERS
+
+// ❕ value
+// 📝 Return a value to the choosen selector.
+// TODO Syntax: ELEMENT.value
+
 
---
+++ JS/chapter_5.js
@@ -0,0 +1,37 @@
+
+/* -------------------------------------------------------------------------- */
+console.log("--- 📁 CHAPTER 5: How JavaScript Works Behind The Scene ---");
+/* -------------------------------------------------------------------------- */
+
+// 🧡: Main topic
+// 📝: Notes
+// ❕: Subtopics
+// 🔗: Sub-subtopics
+// 📦: Variables
+/* -------------------------------------------------------------------------- */
+
+// 🧡 An High-Level Overview of JavaScrip
+
+// -----------------
+// ❕ Monster definition of JavaScript
+
+// JAVASCRIPT IS A HIGH-LEVEL, PROTOTYPE-BASED OBJECT-ORIENTED, MULTI-PARADIGM, INTERPRETED OR JUST-IN-TIME COMPILED,
+// DYNAMIC, SINGLE-THREADED, GARBAGE-COLLECTED PROGRAMMING LANGUAGE WITH FIRST-CLASS FUNCTIONS AND A NON-BLOCKING
+// EVENT LOOP CONCURRENCY MODEL
+
+// -----------------
+// ❕ Deconstructing the monster definition of JavaScript
+
+// 🔗 1. High level
+// 📝 These language have so-called abstractions that will manage our resources automatically.
+// 📝This makes the language easier to learn and to use, but the downside is that programs will never be as fast...
+// or as optimized as for example, C programs.
+
+
+// 🔗 2. Garbage-collected
+// 📝 Garbage-collection is a powerful tool inside the JavaScript engine, which automatically removes...
+// old & unused objects from the computer memory from time to time.
+// 📝 So that we don't have to manage our memory manually.
+
+// 🔗 1. High level
+// 📝 These language have so-called abstractions that will manage our resources automatically.
